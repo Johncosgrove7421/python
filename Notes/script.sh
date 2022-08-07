4db515f5fb0c69e827f7ea151b775f33dd4aabb4
johncosgrove7421 <johncosgrove7421@gmail.com>
2022-08-07 15:11:29 -0400

    chapter notes

---
+++ Notes/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+F1=Python_Basics
+F2=Flow_control
+F3=Functions
+F4=Lists
+F5=Dictionaries_and_Structuring_Data
+F6=Manipulating_Strings
+F7=Pattern_Matching_with_Regular_Expressions
+F8=Input_Validation
+F9=Reading_and_Writing_Files
+F10=Organizing_Files
+F11=Debugging
+F12=Web_Scraping
+F13=Working_with_Excel_Spreadsheets
+F14=Working_with_Google_Spreadsheets
+F15=Working_with_PDF_and_Word_Documents
+F16=Working_with_CSV_Files_and_JSON_Data
+F17=Keeping_Time,Scheduling_Tasks,and_Launching_Programs
+F18=Sending_Email_and_Text_Messages
+F19=Manipulating_Images
+F20=Controlling_the_Keyboard_and_Mouse_with_GUI_Automation
+
+touch $F1
+touch $F2
+touch $F3
+touch $F4
+touch $F5
+touch $F6
+touch $F7
+touch $F8
+touch $F9
+touch $F10
+touch $F11
+touch $F12
+touch $F13
+touch $F14
+touch $F15
+touch $F16
+touch $F17
+touch $F18
+touch $F19
+touch $F20
+
+echo "Go Automate the Boring Stuff with Python"
