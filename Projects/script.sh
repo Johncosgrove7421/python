8baedfda0512439976347dc7815ced6ae8a670c7
Johncosgrove7421 <615619@bah.com>
2022-08-08 17:56:33 -0400

    First program

---
+++ Projects/script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+F1=Python Basics
+F2=Flow control
+F3=Functions
+F4=Lists
+F5=Dictionaries and Structuring Data
+F6=Manipulating Strings
+F7=Pattern Matching with Regular Expressions
+F8=Input Validation
+F9=Reading and Writing Files
+F10=Organizing Files
+F11=Debugging
+F12=Web Scraping
+F13=Working with Excel Spreadsheets
+F14=Working with Google Spreadsheets
+F15=Working with PDF and Word Documents
+F16=Working with CSV Files and JSON Data
+F17=Keeping Time, Scheduling Tasks, and Launching Programs
+F18=Sending Email and Text Messages
+F19=Manipulating Images
+F20=Controlling the Keyboard and Mouse with GUI Automation
+
+touch $F1
+touch $F2
+touch $F3
+touch $F4
+touch $F5
+touch $F6
+touch $F7
+touch $F8
+touch $F9
+touch $F10
+touch $F11
+touch $F12
+touch $F13
+touch $F14
+touch $F15
+touch $F16
+touch $F17
+touch $F18
+touch $F19
+touch $F20
+
+
+echo "You're welcome"
