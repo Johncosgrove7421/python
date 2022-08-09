49f7031e1928c917bd461fd395ad46e3944eaa79
Johncosgrove7421 <615619@bah.com>
2022-08-09 06:19:46 -0400

    for loop + wedding catering calculator

--- Projects/for_loop.py
+++ Projects/for_loop.py
@@ -2,3 +2,7 @@ total = 0
 for num in range(101):
     total = total + num
 print(total)
+
+# print('My name is)
+#for i in range(5, -1, -1):
+#    print('Jimmy Five Times ' + str(i))
---
+++ Projects/wedding_costv2.py
@@ -0,0 +1,54 @@
+#This Python program is used to calculate catering cost
+
+
+
+appetizers = 4
+tablecloths = 13
+cups = .30
+bartenders = 35
+
+
+print('How many people will be attending?')
+p = input ('')
+
+print('How many tables will you have?')
+t = input('')
+
+print("How many cups will the bartenders need to provide?")
+c = input('')
+
+print('How many hours will you need the bartenders for?')
+h = input ('')
+
+food = int(p) * 28 # 24 per person plus 4 for each appetizer
+tablecloths = int(t) * 13
+cups = int(c) * .30
+bartenders = int(h) * 35
+
+x = (food + tablecloths + cups + bartenders)
+
+print(x+(x * .07 )) # this is the total with taxes included
+
+
+
+# example: before taxes 
+#How many people will be attending?
+#60
+#How many tables will you have?
+#8
+#How many cups will the bartenders need to provide?
+#100
+#How many hours will you need the bartenders for?
+#4
+#1954.0
+
+# example: after taxes
+#How many people will be attending?
+#60
+#How many tables will you have?
+#8
+#How many cups will the bartenders need to provide?
+#100
+#How many hours will you need the bartenders for?
+#4
+#2090.78
