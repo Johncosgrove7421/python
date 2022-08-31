65646c7dd69e010fdb07d856cc78a8d1de162c51
John <johncosgrove7421@gmail.com>
2022-08-31 16:07:55 -0400

    random list

---
+++ 100_Days_of_Code/random_list.py
@@ -0,0 +1,12 @@
+# objective is to randomize an integer from the provided
+import random
+
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# x = len(states_of_america)
+# y = random.randint(0, x -1 ) # minus one because it starts with 0
+# print(states_of_america[y])
+
+# or the easy way
+ran = random.choice(states_of_america)
+print(ran)
